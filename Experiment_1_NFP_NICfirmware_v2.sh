9e2abe678af328264cf0b119139ccfd6a62e0bb6
Joydeep Pal <joy.xmofg@gmail.com>
2023-05-17 20:18:38 +0530

    Updated to unify these scripts

--- Experiment_1_NFP_NICfirmware_v2.sh
+++ Experiment_1_NFP_NICfirmware_v2.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # Author: Joydeep Pal
-# Date: 10 Jan 2023
+# Date: Nov 2022
+# Modified: 18 May 2023 (Joydeep Pal)
 # Description: Broadly, it transmits custom traffic of ST and BE
 # defined by VLAN packets to the destination end-host
-# via NFP running our custom Micro-C program.
+# via NFP running NIC firmware (nic_firmware).
 # Use relevant python script to analyze the performance.
 
 # It uses ssh to run remote commands and
 # you should have set up ssh and configured passwordless ssh.
 
 # System Design:
+# Using regular Ethernet port (1GbE) of the hosts because of connection to local switch
 #                               ____________________
 # (PC#1) MultiNIC_TX_port --> | NFP_P0 --> NFP_P1 | --> (PC #2) MultiNIC_RX_port
 #                              ---------------------
@@ -24,29 +26,34 @@ BandwidthBEFlow=6.6M # 10M # 6.6M
 Duration=1
 DurationCapture=$(($Duration+10))
 RemoteIP=10.114.64.248
-LocalCaptureEthernetInterface=enp1s0f0 # enp3s0 #
-#LocalCaptureInterfaceIP=11.11.11.10 # 10.114.64.70 #
-RemoteCaptureEthernetInterface=enp7s0f0 #enp5s0 #
-#RemoteCaptureInterfaceIP=11.11.11.11 # 10.114.64.107 #
+NFPIP=10.114.64.107
+LocalCaptureEthernetInterface=enp1s0f0
+#LocalCaptureInterfaceIP=10.114.64.70
+RemoteCaptureEthernetInterface=enp7s0f0
+#RemoteCaptureInterfaceIP=10.114.64.107
 
 TXfile=/tmp/tx_trial.pcap
 RXfile=/tmp/rx_trial.pcap
 RemoteCapture=$RXfile
 
 #echo 'Step 0a: Configure network namespaces using script, if both tx and rx ports are on same PC'
+echo 'Step 0b: Configure Netronome as a switch using the script'
 
-echo 'Step 1: Start packet capture on tx and rx'
+echo 'Step 1a: Start packet capture on tx and rx'
 ssh zenlab@$RemoteIP "tshark -i $RemoteCaptureEthernetInterface -a duration:$DurationCapture -w $RemoteCapture" &
 tshark -i $LocalCaptureEthernetInterface -a duration:$DurationCapture -w $TXfile &
+
+echo 'Step 1b: Start packet capture on NFP Ethernet ports.'
+ssh zenlab@$NFPIP " \
+tshark -i enp1s0np0 -a duration:$DurationCapture -w /tmp/tx_nfp_p0.pcap && \
+tshark -i enp1s0np1 -a duration:$DurationCapture -w /tmp/tx_nfp_p1.pcap \
+" &
+
 sleep 5
 
 echo 'Step 2: Start packet tx (Mixed VLAN Traffic)'
 tcpreplay -i $LocalCaptureEthernetInterface -M $BandwidthSTFlow --duration=$Duration '../VLAN_PCAP_Files/NewFlows/Traffic_Flow_vlan(2)_packetsize(100B)_Priority(0)_NoPrio_test8.pcap' &
 tcpreplay -i $LocalCaptureEthernetInterface -M $BandwidthBEFlow --duration=$Duration '../VLAN_PCAP_Files/NewFlows/Traffic_Flow_vlan(3)_packetsize(100B)_Priority(0)_NoPrio_test8.pcap'
-#tcpreplay -i $LocalCaptureEthernetInterface -M $BandwidthBEFlow --duration=$Duration '../VLAN_PCAP_Files/NewFlows/Traffic_Flow_vlan(4)_packetsize(1000B)_Priority(0)_NoPrio_test8.pcap' &
-#tcpreplay -i $LocalCaptureEthernetInterface -M $BandwidthSTFlow --duration=$Duration '../VLAN_PCAP_Files/ArchiveFlows/VLAN_2_packets_Size_1000B_NoPrio_test4.pcap' &
-#tcpreplay -i $LocalCaptureEthernetInterface -M $BandwidthBEFlow --duration=$Duration '../VLAN_PCAP_Files/ArchiveFlows/VLAN_3_packets_Size_1000B_NoPrio_test4.pcap' &
-#tcpreplay -i $LocalCaptureEthernetInterface -M $BandwidthTest --duration=$Duration ../VLAN_PCAP_Files/Equally_Mixed_VLAN_packets_fixed_data_length.pcap
 
 echo ' '
 echo 'Step 3: Check if packet capture successful by checking if file exists in remote node'
--- Experiment_2_LocalSwitch.sh
+++ Experiment_2_LocalSwitch.sh
@@ -2,22 +2,30 @@
 
 # Author: Joydeep Pal
 # Date: Oct 2022
-# Description: Broadly, it transmits heavy traffic (using iperf)
-# to the destination end-host via the local switch.
+# Modified: 18 May 2023 (Joydeep Pal)
+# Description: Broadly, it transmits custom traffic of ST and BE
+# defined by VLAN packets to the destination end-host
+# via the local switch.
 # Use relevant python script to analyze the performance.
 
 # It uses ssh to run remote commands and
 # you should have set up ssh and configured passwordless ssh.
 
+# System Design:
+# Using regular Ethernet port (1GbE) of the hosts because of connection to local switch
+#                               ____________________
+# (PC#1) MultiNIC_TX_port --> | LocalSwitch_P0 --> LocalSwitch_P1 | --> (PC #2) MultiNIC_RX_port
+#                              ---------------------
+
+whoami
+
 # Script parameters:
 BandwidthTest=100M
-PacketSize=1000
-Duration=5
-DurationCapture=$(($Duration+5))
-ParallelStreams=1
-
+BandwidthSTFlow=6.6M # 10M # 6.6M
+BandwidthBEFlow=6.6M # 10M # 6.6M
+Duration=1
+DurationCapture=$(($Duration+10))
 RemoteIP=10.114.64.107
-
 LocalCaptureEthernetInterface=enp3s0
 LocalCaptureInterfaceIP=10.114.64.70
 RemoteCaptureEthernetInterface=enp7s0
@@ -26,39 +34,35 @@ Port=4000
 
 TXfile=/tmp/tx_trial.pcap
 RXfile=/tmp/rx_trial.pcap
-RemoteCapture=$TXfile
+RemoteCapture=$RXfile
 
-# System Design:
-# Using regular Ethernet port (1GbE) of the hosts because of connection to local switch
-# (PC#1) MultiNIC_TX_port --> LocalSwitch_P0 --> LocalSwitch_P1 --> (PC #2) MultiNIC_RX_port
+#echo 'Step 0a: Configure network namespaces using script, if both tx and rx ports are on same PC'
 
-echo 'Step 1: Start packet rx (iperf server) on local node'
-iperf -s -u -p $Port &
-
-echo 'Step 2: Start packet capture on tx and rx nodes'
-tshark -i $LocalCaptureEthernetInterface -a duration:$DurationCapture -w $RXfile &
+echo 'Step 1a: Start packet capture on tx and rx'
 ssh zenlab@$RemoteIP "tshark -i $RemoteCaptureEthernetInterface -a duration:$DurationCapture -w $RemoteCapture" &
+tshark -i $LocalCaptureEthernetInterface -a duration:$DurationCapture -w $TXfile &
 sleep 5
 
-echo 'Step 3: Start packet tx (iperf client) on remote node'
-ssh zenlab@$RemoteIP " \
-iperf -u -c $LocalCaptureInterfaceIP -t $Duration -p $Port -b $BandwidthTest -P $ParallelStreams && #>/dev/null
-#iperf3 -u -c $LocalCaptureInterfaceIP -t $Duration -p $Port -b $BandwidthTest -l $PacketSize &&
-ls /tmp/ | grep tx \
-"
+echo 'Step 2: Start packet tx (Mixed VLAN Traffic)'
+tcpreplay -i $LocalCaptureEthernetInterface -M $BandwidthSTFlow --duration=$Duration '../VLAN_PCAP_Files/NewFlows/Traffic_Flow_vlan(2)_packetsize(100B)_Priority(0)_NoPrio_test8.pcap' &
+tcpreplay -i $LocalCaptureEthernetInterface -M $BandwidthBEFlow --duration=$Duration '../VLAN_PCAP_Files/NewFlows/Traffic_Flow_vlan(3)_packetsize(100B)_Priority(0)_NoPrio_test8.pcap'
 
-echo 'Step 4: Transfer Tx capture file from remote to local system for analysis'
-sleep 8
-scp -C zenlab@$RemoteIP:$RemoteCapture $TXfile
+echo ' '
+echo 'Step 3: Check if packet capture successful by checking if file exists in remote node'
+sleep 10
+ssh zenlab@$RemoteIP "ls /tmp/ | grep rx"
 
-echo 'Step 5: Convert pcap to csv'
-args="-T fields -E header=y -E separator=, \
--e ip.id -e ip.src -e ip.dst -e udp.srcport -e udp.dstport \
--e frame.time_epoch -e udp.length.bad -e udp.length -e data.len"
-eval tshark -r $TXfile $args -Y 'udp.dstport==4000' > ../TempCSVfiles/TXv1.csv &
-eval tshark -r $RXfile $args -Y 'udp.dstport==4000' > ../TempCSVfiles/RXv1.csv
+echo 'Step 4: Transfer Tx capture from remote to local system for analysis'
+scp -C zenlab@$RemoteIP:$RemoteCapture $RXfile
+
+echo 'Step 5: Convert pcap to csv for automated analysis with python'
+args="-T fields -E header=y -E separator=, -e ip.src -e ip.dst -e ip.id -e vlan.id -e vlan.priority \
+-e udp.srcport -e udp.dstport -e frame.time_epoch -e frame.len -e udp.length.bad -e udp.length -e data.len"
+eval tshark -r $TXfile $args -Y 'udp.dstport==4000' > ../TempCSVfiles/RXv1.csv &
+eval tshark -r $RXfile $args -Y 'udp.dstport==4000' > ../TempCSVfiles/TXv1.csv
 
 echo 'Step 6: Process using python script'
+./Latency_v5.py $BandwidthSTFlow $BandwidthBEFlow $Duration
 
-killall iperf
 echo 'Done !!'
+echo ' '
